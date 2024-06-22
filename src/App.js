0980b780f270edc2ddec017b6c7d8613943e02d0
co44upt1ocvf <corruold@gmail.com>
2024-06-22 05:16:54 +0300

    tc

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Block2 from './components/Block2';
 import Block3 from './components/Block3';
 import Footer from './components/Footer';
 import './App.css';
+import './styles/Container.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Title />
-      <Block1 />
-      <Block2 />
-      <Block3 />
-      <Footer />
+      <div className='container'>
+        <Title />
+        <Block1 />
+        <Block2 />
+        <Block3 />
+        <Footer />
+      </div>
     </div>
   );
 }
